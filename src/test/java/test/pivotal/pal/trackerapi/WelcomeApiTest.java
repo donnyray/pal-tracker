c5636a0da64d77d87e5df00998c50296c8e5cfd0
Travis Moretz <travis.moretz@gmail.com>
2018-06-14 12:36:08 -0600

    Enable security

--- src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
@@ -1,11 +1,14 @@
 package test.pivotal.pal.trackerapi;
 
 import io.pivotal.pal.tracker.PalTrackerApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,8 +19,19 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 public class WelcomeApiTest {
 
     // some random comment
+    @LocalServerPort
+    private String port;
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Before
+    public void setUp() throws Exception {
+        RestTemplateBuilder builder = new RestTemplateBuilder()
+                .rootUri("http://localhost:" + port)
+                .basicAuthorization("user", "password");
+
+        restTemplate = new TestRestTemplate(builder);
+    }
 
     @Test
     public void exampleTest() {
